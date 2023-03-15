8f12d92d40c0c272a8005697e66305ad51f8e508
Stephen Duncanson <Stephen.duncanson@gmail.com>
2023-03-14 21:33:14 -0400

    Add project files.

---
+++ Hockanum/Models/CylindricalTank.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hockanum.Models
+{
+    public partial class CylindricalTank : ObservableObject
+    {
+        /// <summary> Meters. Distance from the center of the tank to the edge of the tank. </summary>
+        public double Radius { get; }
+        
+        /// <summary> Meters. Distance from the base of the tank to the top of the tank. </summary>
+        public double Height { get; }
+
+        /// <summary> Meters^3. Total volume of the cylindrical tank. </summary>
+        public double TotalVolume { get; }
+
+        [ObservableProperty]
+        private double filledVolume;
+
+        partial void OnFilledVolumeChanged(double value)
+        {
+            if (FilledVolume > TotalVolume) FilledVolume = TotalVolume;
+            else if (FilledVolume < 0) FilledVolume = 0;
+        }
+
+        public CylindricalTank(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+            TotalVolume = Math.PI * radius * radius * height;
+        }
+
+    }
+}
---
+++ Hockanum/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Hockanum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hockanum.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private CylindricalTank equalizationTank = new CylindricalTank(1, 3);
+
+        [RelayCommand]
+        private void AddCubicMeter() { equalizationTank.FilledVolume += 1; }
+
+        [RelayCommand]
+        private void RemoveCubicMeter() { equalizationTank.FilledVolume -= 1; }
+
+    }
+}
